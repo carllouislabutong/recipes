b7295ed4f117af54fbfac9c7b013477874369a4b
unknown <carllouislabutong@gmail.com>
2022-05-14 08:18:35 +0800

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful"
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from "../../components/Skeleton";
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken:  process.env.CONTENTFUL_ACCESS_KEY,
@@ -29,6 +30,7 @@ export async function getStaticProps({params}){
 
 
 export default function RecipeDetails({recipe}) {
+  if (!recipe) return <Skeleton />
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
   return (
     <div>
